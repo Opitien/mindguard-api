fdeb516957dfeacee6bf454a4563e40cae0019fd
Opitien Ejiroghene <opitiene@gmail.com>
2025-11-20 02:40:05 -0800

    Downgrade React to 18 for vaul compatibility

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\themadafghan\Downloads\mindguard-api\app\layout.tsx
+// File: C:\Users\themadafghan\mindguard-api\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
